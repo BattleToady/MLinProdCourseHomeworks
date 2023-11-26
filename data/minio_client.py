9098d5feff75c07fefbb7449ce6700d00bc6bcf5
Vladislav <goldovanskijvladislav@gmail.com>
2023-11-26 03:08:28 +0200

    Adding decorator to minio_client functions

--- data/minio_client.py
+++ data/minio_client.py
@@ -10,50 +10,40 @@ minio_client = Minio(
     secure=False  
 )
 
+def response_error_decorator(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+            return 0
+        except ResponseError as err:
+            print(err)
+            return 1
+    return wrapper
+
+@response_error_decorator
 def create_bucket(bucket_name):
-    try:
-        if not minio_client.bucket_exists(bucket_name):
-            minio_client.make_bucket(bucket_name)
-            print(f"Bucket '{bucket_name}' created successfully")
-        else:
-            print(f"Bucket '{bucket_name}' already exists")
-        return 0
-    except ResponseError as err:
-        print(err)
-        return 1
+    if not minio_client.bucket_exists(bucket_name):
+        minio_client.make_bucket(bucket_name)
+        print(f"Bucket '{bucket_name}' created successfully")
+    else:
+        print(f"Bucket '{bucket_name}' already exists")
 
+@response_error_decorator
 def upload_file(bucket_name, object_name, file_path):
-    try:
-        minio_client.fput_object(bucket_name, object_name, file_path)
-        print(f"File '{object_name}' uploaded to '{bucket_name}' successfully")
-        return 0
-    except ResponseError as err:
-        print(err)
-        return 1
+    minio_client.fput_object(bucket_name, object_name, file_path)
+    print(f"File '{object_name}' uploaded to '{bucket_name}' successfully")
 
+@response_error_decorator
 def download_file(bucket_name, object_name, local_file_path):
-    try:
-        minio_client.fget_object(bucket_name, object_name, local_file_path)
-        print(f"File '{object_name}' downloaded from '{bucket_name}' successfully")
-        return 0
-    except ResponseError as err:
-        print(err)
-        return 1
+    minio_client.fget_object(bucket_name, object_name, local_file_path)
+    print(f"File '{object_name}' downloaded from '{bucket_name}' successfully")
 
+@response_error_decorator
 def delete_file(bucket_name, object_name):
-    try:
-        minio_client.remove_object(bucket_name, object_name)
-        print(f"File '{object_name}' deleted from '{bucket_name}' successfully")
-        return 0
-    except ResponseError as err:
-        print(err)
-        return 1
+    minio_client.remove_object(bucket_name, object_name)
+    print(f"File '{object_name}' deleted from '{bucket_name}' successfully")
 
+@response_error_decorator
 def delete_bucket(bucket_name):
-    try:
-        minio_client.remove_bucket(bucket_name)
-        print(f"Bucket '{bucket_name}' deleted successfully")
-        return 0
-    except ResponseError as err:
-        print(err)
-        return 1
+    minio_client.remove_bucket(bucket_name)
+    print(f"Bucket '{bucket_name}' deleted successfully")
